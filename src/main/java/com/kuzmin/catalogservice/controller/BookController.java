ad6477caede1acdb0f1e2e62900aef5f96575528
kuzminal <kuzmin.al35@gmail.com>
2020-12-20 20:46:29 +0300

    Added validation and Controller advise for error handling

--- src/main/java/com/kuzmin/catalogservice/controller/BookController.java
+++ src/main/java/com/kuzmin/catalogservice/controller/BookController.java
@@ -4,33 +4,40 @@ import com.kuzmin.catalogservice.model.Book;
 import com.kuzmin.catalogservice.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
     private final BookService bookService;
+
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
     @GetMapping
     public Collection<Book> get() {
         return bookService.viewBookList();
     }
+
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
         return bookService.viewBookDetails(isbn).orElse(null);
     }
+
     @PostMapping
-    public Book post(@RequestBody Book book) {
+    public Book post(@Valid @RequestBody Book book) {
         return bookService.addBookToCatalog(book);
     }
+
     @DeleteMapping("{isbn}")
     public Book delete(@PathVariable String isbn) {
         return bookService.removeBookFromCatalog(isbn);
     }
+
     @PutMapping("{isbn}")
-    public Book put(@PathVariable String isbn, @RequestBody Book book) {
+    public Book put(@Valid @PathVariable String isbn, @RequestBody Book book) {
         return bookService.editBookDetails(isbn, book);
     }
 }
--- src/main/java/com/kuzmin/catalogservice/model/Book.java
+++ src/main/java/com/kuzmin/catalogservice/model/Book.java
@@ -5,14 +5,23 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
 import java.time.Year;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class Book {
+    @NotBlank(message = "The book ISBN must be defined.")
+    @Pattern(regexp = "^(97([89]))?\\d{9}(\\d|X)$",
+            message = "The ISBN format must follow the standards ISBN-10or ISBN-13.")
     private String isbn;
+    @NotBlank(message = "The book title must be defined.")
     private String title;
+    @NotBlank(message = "The book author must be defined.")
     private String author;
+    @PastOrPresent(message = "The book cannot have been published in the future.")
     private Year publishingYear;
 }
