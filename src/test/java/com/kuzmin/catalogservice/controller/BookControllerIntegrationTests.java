5c59130488093d2c5fe8c68b98429e8821ea0b40
kuzminal <kuzmin.al35@gmail.com>
2020-12-23 21:44:40 +0300

    Tried to use testcontainers with postgres

--- src/test/java/com/kuzmin/catalogservice/controller/BookControllerIntegrationTests.java
+++ src/test/java/com/kuzmin/catalogservice/controller/BookControllerIntegrationTests.java
@@ -7,23 +7,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Year;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 public class BookControllerIntegrationTests {
     @Autowired
     private TestRestTemplate restTemplate;
+
     @Test
-    void whenPostRequestThenBookCreated() {
-        Book expectedBook = new Book("1231231231", "Title", "Author", Year.of(1991), 1.5);
-        ResponseEntity<Book> response = restTemplate.postForEntity("/books", expectedBook,
-                Book.class);
+    void whenGetRequestWithIdThenBookReturned() {
+        String bookIsbn = "1231231230";
+        Book expectedBook = new Book(bookIsbn, "Book Title", "Book Author", Year.of(1991), 9.90);
+        restTemplate.postForEntity("/books", expectedBook, Book.class);
+        ResponseEntity<Book> response = restTemplate.getForEntity("/books/" + bookIsbn, Book.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody())
-                .isNotNull()
-                .isEqualToComparingFieldByField(expectedBook);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getIsbn()).isEqualTo(bookIsbn);
     }
 }
