a7abb0945529faa8094119cf924a7efe9642e56c
kuzminal <kuzmin.al35@gmail.com>
2021-01-11 10:57:15 +0300

    dockerization

--- src/main/java/com/kuzmin/catalogservice/model/BaseEntity.java
+++ src/main/java/com/kuzmin/catalogservice/model/BaseEntity.java
@@ -1,12 +1,10 @@
 package com.kuzmin.catalogservice.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
